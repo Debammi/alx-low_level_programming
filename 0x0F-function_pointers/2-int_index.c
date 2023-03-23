3c7a1e7624a8ab692bcc1e3b712fdbf28e684560
Lydia_Ajibade <oluwatoyinlydia3@gmail.com>
2023-03-23 13:45:04 -0700

    tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,10 +15,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
-	for (1=0; i < size; i++)
+	for (1 = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
-			return(i);
+			return (i);
 	}
-	return(-1);
+	return (-1);
 }
