42565875c98b73bf6af03f33cd8be70e69cbcdc1
Lydia_Ajibade <oluwatoyinlydia3@gmail.com>
2023-03-23 14:25:04 -0700

    tasks

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 /**
 * get_op_func - ...
 * @s: ...
@@ -11,26 +10,21 @@
 */
 int (*get_op_func(char *s))(int, int)
 {
-   op_t ops[] = {
-       {"+", op_add},
-       {"-", op_sub},
-       {"*", op_mul},
-       {"/", op_div},
-       {"%", op_mod},
-       {NULL, NULL}};
-   int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i = 0;
 
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 
-   while (i < 5)
-   {
-       if (strcmp(s, ops[i].op) == 0)
-           return (ops[i].f);
-
-
-       i++;
-   }
-
-
-   return (0);
+		i++;
+	}
+	return (0);
 }
-
